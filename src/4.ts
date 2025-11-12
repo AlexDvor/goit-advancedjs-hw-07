605a165943c0a4d6264440c1e0f59ded693fa9e1
AlexDvor <alexmatvichuk@gmail.com>
2025-11-12 19:23:35 +0100

    add 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,65 @@
-const key = new Key();
+class Key {
+	private signature: number
 
-const house = new MyHouse(key);
-const person = new Person(key);
+	constructor() {
+		this.signature = Math.random()
+	}
 
-house.openDoor(person.getKey());
+	getSignature() {
+		return this.signature
+	}
+}
 
-house.comeIn(person);
+class Person {
+	private key: Key
+	constructor(value: Key) {
+		this.key = value
+	}
 
+	getKey(): Key {
+		return this.key
+	}
+}
 
-export {};
+abstract class House {
+	protected door: boolean = false
+	protected key: Key
+	protected tenants: Person[] = []
+
+	constructor(key: Key) {
+		this.key = key
+	}
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person)
+			console.log('Людина зайшла в дім.')
+		} else {
+			console.log('Двері зачинені. Людина не може зайти.')
+		}
+	}
+
+	abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true
+			console.log('Двері відкриті.')
+		} else {
+			console.log('Ключ не підходить.')
+		}
+	}
+}
+
+const key = new Key()
+
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+export {}
